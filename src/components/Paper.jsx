c3c6eabd1b109eed09c1e29624e34b583e17d9ed
Ernie Turner <ernie.turner@ironcorelabs.com>
2018-07-20 15:48:10 -0600

    Minor fixes after looking at code in PR

--- src/components/Paper.jsx
+++ src/components/Paper.jsx
@@ -7,7 +7,7 @@ const classes = stylesListToClassNames({
         padding: 15,
         overflow: "hidden",
         margin: 10,
-        boxShadow: "0px 1px 5px 0px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 3px 1px -2px rgba(0, 0, 0, 0.12)",
+        boxShadow: "0 1px 5px 0 rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.12)",
         backgroundColor: "#fff",
         wordBreak: "break-word",
         borderRadius: 5,
